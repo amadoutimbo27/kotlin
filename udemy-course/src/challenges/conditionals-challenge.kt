2c17139dedd2bf50c684f2b8c70b6323f0b879cc
Amadou Camara <38413486+amadoutimbo27@users.noreply.github.com>
2018-04-15 19:49:28 -0700

    Update conditionals-challenge.kt

--- udemy-course/src/challenges/conditionals-challenge.kt
+++ udemy-course/src/challenges/conditionals-challenge.kt
@@ -9,13 +9,30 @@ import java.util.*
  * Generate a random number between 0 and 49 and checks in which range it is.
  */
 fun main(args: Array<String>) {
-    val random = Random().nextInt(50) + 1
-
-    when (random) {
-        in 1..10 -> println("In 1 to 10: $random")
-        in 11..20 -> println("In 11 to 20: $random")
-        in 21..30 -> println("In 21 to 30: $random")
-        in 31..40 -> println("In 31 to 40: $random")
-        else -> println("Over 40: $random")
+    val random = Random().nextInt( 50) +1
+    when(random){
+        in 1..10 -> {
+            println("The number is $random.")
+            println("This random number is between 1 to 10.")
+        }
+        in 11..20 -> {
+            println("The number is $random.")
+            println("This random number is between 11 to 20.")
+        }
+        in 21..30 -> {
+            println("The number is $random.")
+            println("This random number is between 21 to 30.")
+        }
+        in 31..40 -> {
+            println("The number is $random.")
+            println("This random number is between 31 to 40.")
+        }
+        in 41..50 -> {
+            println("The number is $random.")
+            println("This random number is between 41 to 50")
+        }
+        else -> println("This random number is out of bounds.")
     }
+    println()
+    println("*******Done*******")
 }
